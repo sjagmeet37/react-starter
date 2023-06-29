a1b54e8a87d6c5fed4d9509e08e6ab3738f6cced
Jagmeet Sandhu <sjagmeet37@gmail.com>
2023-06-29 17:24:18 +0530

    blank commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Let's begin react
         </a>
       </header>
     </div>
