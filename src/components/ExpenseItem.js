697acc8793435cd580d31fc92d5e1a973497f454
Jagmeet Sandhu <sjagmeet37@gmail.com>
2023-06-30 16:49:20 +0530

    adding css to expense component and externalizing variables

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,15 @@
+import "./ExpenseItem.css"
 function ExpenseItem() {
 
-    return (
-        <div>
-            <div>June 30</div>
-            <div>
-                <h2>Hello</h2>
-                <div>$234</div>
+    let expenseDate = new Date();
+    let expenseName = "Fuel";
+    let expenseAmount = 290
+;    return (
+        <div className="expense-item">
+            <div>{expenseDate.toISOString()}</div>
+            <div  className="expense-item__description">
+                <h2>{expenseName}</h2>
+                <div className="expense-item__price">${expenseAmount}</div>
             </div>
         </div>
     );
