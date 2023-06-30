b328858939f7ed58ea652cf890e3c6923d875fd5
Jagmeet Sandhu <sjagmeet37@gmail.com>
2023-06-30 16:32:17 +0530

    expense component

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,14 @@
+function ExpenseItem() {
+
+    return (
+        <div>
+            <div>June 30</div>
+            <div>
+                <h2>Hello</h2>
+                <div>$234</div>
+            </div>
+        </div>
+    );
+}
+
+export default ExpenseItem
