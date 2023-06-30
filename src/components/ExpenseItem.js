aa8f09d7cbd180dd34fc68ae3ba0bed3c37b9f88
Jagmeet Sandhu <sjagmeet37@gmail.com>
2023-06-30 20:58:54 +0530

    updated date divs

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,22 @@
-import "./ExpenseItem.css"
+import "./ExpenseItem.css";
 function ExpenseItem(props) {
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
 
-    
-;    return (
-        <div className="expense-item">
-            <div>{props.date.toISOString()}</div>
-            <div  className="expense-item__description">
-                <h2>{props.name}</h2>
-                <div className="expense-item__price">${props.amount}</div>
-            </div>
-        </div>
-    );
+  return (
+    <div className="expense-item">
+      <div>
+        <div>{month}</div>
+        <div>{day}</div>
+        <div>{year}</div>
+      </div>
+      <div className="expense-item__description">
+        <h2>{props.name}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </div>
+  );
 }
 
-export default ExpenseItem
+export default ExpenseItem;
