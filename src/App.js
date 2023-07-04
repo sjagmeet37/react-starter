e947eadeff906a4179c796406885860480bc4e3d
Jagmeet Sandhu <sjagmeet37@gmail.com>
2023-07-04 20:19:05 +0530

    adding Form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 function App() {
   let expenses = [
     { id: "1", title: "title1", date: new Date("2023-06-30"), amount: 100 },
@@ -15,6 +16,7 @@ function App() {
   
   return (
     <div className="App">
+      <NewExpense />
       <Expenses 
         expenses = {expenses}/>      
     </div>
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -12,6 +12,7 @@ function ExpenseItem(props) {
     console.log("Clicked !!!!!!")
   }
 
+  setTimeout(() => {setName('Timout Name')}, 10000);
   return (
     <Cards className="expense-item">
         <ExpenseDate 
