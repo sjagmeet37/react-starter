692b52b613a5b09ba832f4dcdef69248838777ac
Jagmeet Sandhu <sjagmeet37@gmail.com>
2023-07-04 19:02:52 +0530

    using states

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,17 +2,24 @@ import "./ExpenseItem.css";
 
 import ExpenseDate from "./ExpenseDate";
 import Cards from "../UI/Cards";
+import { useState } from "react";
 
 function ExpenseItem(props) {
  
+  const [name, setName] = useState(props.name);
+  const clickHandler = function() {
+    setName("Updated name");
+    console.log("Clicked !!!!!!")
+  }
 
   return (
     <Cards className="expense-item">
         <ExpenseDate 
         date={props.date}/>
       <div className="expense-item__description">
-        <h2>{props.name}</h2>
+        <h2>{name}</h2>
         <div className="expense-item__price">${props.amount}</div>
+        <button onClick={clickHandler}>Click for Event</button>
       </div>
     </Cards>
   );
